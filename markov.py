3e9dae8f1045c18e33426950301aa5b20c2897b6
Ryan Geary <rtgnj42@gmail.com>
2020-02-26 16:39:01 -0500

    Ingest files and generate sentences

--- markov.py
+++ markov.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-from functools import *
+import random
+import re
+
+random.seed()
+
 
 class MarkovNode(object):
     def __init__(self, word):
@@ -21,14 +25,21 @@ class MarkovNode(object):
     def link_total(self):
         return sum(self.next_words.values(), 0)
 
+    def choose_next(self):
+        select = []
+        for word in self.next_words:
+            for i in range(self.next_words[word]):
+                select.append(word)
+        return select[random.randint(0, len(select)-1)]
+
+
 class MarkovNodeList():
-    START = "START_OF_SENTENCE"
-    END = "END_OF_SENTENCE"
+    BARRIER = "SENTENCE_BARRIER"
 
     def __init__(self):
         # dict of { "word": MarkovNode }
         self.words = {}
-        self.words[MarkovNodeList.START] = MarkovNode(MarkovNodeList.START)
+        self.words[MarkovNodeList.BARRIER] = MarkovNode(MarkovNodeList.BARRIER)
 
     def __str__(self):
         out = ""
@@ -47,6 +58,43 @@ class MarkovNodeList():
     def get_words_of_length(self, length):
         return [a for a in self.words.keys() if len(a) == length]
 
+
+def ingest_file(file_name):
+    with open(file_name) as f:
+        content = f.read()
+
+    content = content.lower()
+    content = re.sub(r'([^a-z0-9\'\. ])', r' ', content)
+    content = re.sub(r'(\b\' | \'\b)', r' ', content)
+    content = re.sub(r'\.', r' . ', content)
+
+    words = content.split()
+    myMNL = MarkovNodeList()
+    prev_word = MarkovNodeList.BARRIER
+    for cur_word in words:
+        if cur_word == '.':
+            myMNL.increase_link(prev_word, MarkovNodeList.BARRIER)
+            prev_word = MarkovNodeList.BARRIER
+        else:
+            myMNL.increase_link(prev_word, cur_word)
+            prev_word = cur_word
+    return myMNL
+
+
+def generate(mnl, length):
+    cur_word = MarkovNodeList.BARRIER
+    out = ""
+    for _ in range(length):
+        next_word = mnl.words[cur_word].choose_next()
+        out += next_word + " "
+        cur_word = next_word
+    print(out)
+
+
+mnl1 = ingest_file("alice.txt")
+generate(mnl1, 100)
+
+
 # test MarkovNode
 m = MarkovNode("test")
 m.increase_link("anna")
@@ -54,6 +102,7 @@ m.increase_link("anna")
 m.increase_link("anna")
 m.increase_link("jared")
 m.increase_link("jared")
+m.choose_next()
 assert m.word == "test"
 assert len(m.next_words) == 2
 assert m.next_words["anna"] == 3
@@ -62,12 +111,12 @@ assert m.link_total() == 5
 
 # test MarkovNodeList
 mnl = MarkovNodeList()
-mnl.increase_link(MarkovNodeList.START, "poke")
+mnl.increase_link(MarkovNodeList.BARRIER, "poke")
 mnl.increase_link("poke", "anna")
 mnl.increase_link("anna", "and")
 mnl.increase_link("and", "then")
 mnl.increase_link("then", "poke")
 mnl.increase_link("poke", "jared")
-mnl.increase_link("jared", MarkovNodeList.END)
+mnl.increase_link("jared", MarkovNodeList.BARRIER)
 
 assert mnl.words["poke"].next_words == {"anna": 1, "jared": 1}
